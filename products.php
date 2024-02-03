292cabc2d707dc94759be9deaa556010869ddea8
Guy-Philippe Uberos <gpuberos.pro@gmail.com>
2024-02-03 15:28:00 +0100

    fix: remove duplicate products page

--- products.php
+++
@@ -1,6 +0,0 @@
-<?php require_once __DIR__ . '/data/products.data.php'; ?>
-<?php require_once __DIR__ . '/utilities/layout/header.ut.php'; ?>
-
-<?php require_once __DIR__ . '/utilities/card/products_card.ut.php'; ?>
-
-<?php require_once __DIR__ . '/utilities/layout/footer.ut.php'; ?>
