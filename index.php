375274662323ad2ea6b7a2fc7ada6895679565a2
Guy-Philippe Uberos <gpuberos.pro@gmail.com>
2024-02-05 15:01:46 +0100

    add: remove file with table

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
-<?php
-require_once __DIR__ . '/data/doctors.data.php';
-require_once __DIR__ . '/utilities/layout/header.ut.php';
-
-?>
+<?php require_once __DIR__ . '/utilities/layout/header.ut.php'; ?>
 
 <!-- Section générique titre + paragraphe -->
 <div class="container my-5">
